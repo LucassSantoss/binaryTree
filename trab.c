2e9ad3227e417e1d6176d0a9fa82ac77e6b74a9c
Bruno Mascioli <souzamasciolibruno@gmail.com>
2024-04-27 18:06:41 -0300

    fix create_tree and slice_tree

--- trab.c
+++ trab.c
@@ -8,143 +8,153 @@ Bruno Mascioli
 #include <stdlib.h>
 #include <string.h>
 
+#define COUNT 10
+
 typedef struct _Node {
   char item;
-  struct _Node *left;
-  struct _Node *right;
+  struct _Node* left;
+  struct _Node* right;
 } Node;
 
 typedef struct {
-  Node *root;
+  Node* root;
 } t_binary_tree;
 
-Node *createNode(char item) {
-  Node *newNode = (Node *)malloc(sizeof(Node));
+Node* createNode(char item) {
+  Node* newNode = (Node*) malloc(sizeof(Node));
   if (newNode == NULL) {
     fprintf(stderr, "Erro ao alocar memória.\n");
     exit(1);
   }
   newNode->item = item;
-  newNode->left = newNode->right = NULL;
+  newNode->left = NULL;
+  newNode->right = NULL;
   return newNode;
 }
 
 void slice_tree(char line[], char** left_tree, char** right_tree) {
+    char* token = strtok(line, ",");
 
-  char* token = strtok(line, " ");
+    token = strtok(NULL, "\n");
 
-  token = strtok(NULL, "\n");
+    int count_parenteses = 0;
+    int i = 0;
+    int y = 0;
 
-  int count_parenteses = 0;
-  int i = 0;
-  int y = 0;
-
-  while (1) {
-    if (token[i] == '(') {
-      count_parenteses++;
+    while (1) {
+        if (token[i] == '(') 
+            count_parenteses++;
+        if (token[i] == ')') 
+            count_parenteses--;
+        
+        i++;
+        
+        if (count_parenteses == 0 && y == 0) {
+            *left_tree = (char*)malloc(i + 1);
+            strncpy(*left_tree, token, i);
+            (*left_tree)[i] = '\0';
+            y = i + 2; 
+            continue;
+        }
+        
+        if (count_parenteses == 0 && y != 0 && i > y) {
+            *right_tree = (char*)malloc((i - y + 1) * sizeof(char));
+            strncpy(*right_tree, token + y, i - y);
+            (*right_tree)[i - y] = '\0';
+            break;
+        }
     }
-    if (token[i] == ')') {
-      count_parenteses--;
-    }
-    i++;
-    if (count_parenteses == 0 && y == 0) {
-      *left_tree = (char*)malloc(i + 1);
-      strncpy(*left_tree, token, i);
-      (*left_tree)[i] = '\0';
-      y = i + 3;
-      continue;
-    }
-    if(count_parenteses == 0 && y != 0 && i > y){
-      *right_tree = (char*)malloc((i - 1) * sizeof(char));
-      strncpy(*right_tree, token + y - 1, i - y + 1);
-      (*right_tree)[i] = '\0';
-      break;
-    }
-  }
 }
 
-t_binary_tree *create_tree(Node* root, char node_as_string[]) {
-  if (strcmp(node_as_string, "()") == 0) {
-    return NULL;
-  }
 
+Node* create_tree(Node* root, char tree_as_string[]) {
+  if (strcmp(tree_as_string, "()") == 0) return NULL;
+  
   char* left_tree = NULL;
   char* right_tree = NULL;
+  char root_elem = tree_as_string[1];
 
-  slice_tree(node_as_string, &left_tree, &right_tree);
-  printf("Left: %s\n", left_tree);
-  printf("Right: %s\n", right_tree);
-
-  char left_elem = left_tree[1];
-  char right_elem = right_tree[1];
-  char root_elem = node_as_string[1];
   root = createNode(root_elem);
+  slice_tree(tree_as_string, &left_tree, &right_tree);
 
-  root->left = createNode(left_elem);
-  root->right = createNode(right_elem);
-  
-  create_tree(root->left, left_tree); 
-  create_tree(root->right, right_tree);
+  if(strcmp(left_tree, "()") != 0){
+    char left_elem = left_tree[1];
+    root->left = create_tree(root->left, left_tree); 
+    free(left_tree); 
+  }
+
+  if(strcmp(right_tree, "()") != 0){
+    char right_elem = right_tree[1];
+    root->right = create_tree(root->right, right_tree);
+    free(right_tree);
+  }
+
+  return root;
 }
 
-void print(Node *root) {}
 
-void in(Node *root) {
-  if (root == NULL) {
-    printf("A árvore ainda não foi criada!\n");
-    return;
+void print_tree(Node* root, int space) {
+  if(root == NULL) return;
+
+  space += COUNT;
+
+  print_tree(root->right, space);
+
+  printf("\n");
+  for(int i = COUNT; i < space; i++){
+    printf(" ");
   }
+  printf("%c\n", root->item);
+
+  print_tree(root->left, space);
+}
+
+void in(Node* root) {
+  if (root == NULL) return;
   in(root->left);
-  printf("%d ", root->item);
+  printf("%c ", root->item);
   in(root->right);
 }
 
-void pre(Node *root) {
-  if (root == NULL) {
-    printf("A árvore ainda não foi criada!\n");
-    return;
-  }
-  printf("%d ", root->item);
+void pre(Node* root) {
+  if (root == NULL)  return;
+  printf("%c ", root->item);
   pre(root->left);
   pre(root->right);
 }
 
-void pos(Node *root) {
-  if (root == NULL) {
-    printf("A árvore ainda não foi criada!\n");
-    return;
-  }
-  pos(root->left);
-  pos(root->right);
-  printf("%d ", root->item);
+void post(Node* root) {
+  if (root == NULL) return;
+  post(root->left);
+  post(root->right);
+  printf("%c ", root->item);
 }
 
 int main(int argc, char const *argv[]) {
-  t_binary_tree *tree = (t_binary_tree *)malloc(sizeof(t_binary_tree));
+  t_binary_tree* tree = (t_binary_tree*) malloc(sizeof(t_binary_tree));
   tree->root = NULL;
 
-  char line[100];
-  fgets(line, 100, stdin);
+  char line[1000];
+  fgets(line, 1000, stdin);
   while (strcmp(line, "exit") != 0) {
-    char *token = strtok(line, " ");
-
+    char* token = strtok(line, " ");
     if (strcmp(token, "create") == 0) {
       token = strtok(NULL, "\n");
-      printf("%s\n", token);
-      tree = create_tree(tree->root, token);
+      tree->root = create_tree(tree->root, token);
     } else if (strcmp(line, "print") == 0) {
-      print(tree->root);
+      print_tree(tree->root, 0);
     } else if (strcmp(line, "in") == 0) {
       in(tree->root);
+      printf("\n");
     } else if (strcmp(line, "pre") == 0) {
       pre(tree->root);
-    } else if (strcmp(line, "pos") == 0) {
-      pos(tree->root);
+      printf("\n");
+    } else if (strcmp(line, "post") == 0) {
+      post(tree->root);
+      printf("\n");
     }
     fscanf(stdin, "%s", line);
   }
+
   return 0;
 }
-// (A, (B, (), ()))
-
-// (a, (b, (k, (), ()), (j, (), ())), (c, (f, (), ()), (g, (), ())))
